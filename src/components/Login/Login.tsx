55293049eed666353bc18353a19fe0e2edc29d41
khoa <khoanono963@gmail.com>
2025-06-18 08:21:33 +0700

    up

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -134,7 +134,7 @@ export default function Login() {
         <Image
           preview={false}
           alt=""
-          src="/logo1.png"
+          src="/logo2.png"
           className=" h-full "
           height={80}
         />
@@ -146,7 +146,7 @@ export default function Login() {
           <Image
             preview={false}
             alt=""
-            src="/logo1.png"
+            src="/logo2.png"
             className=" h-full w-full xl:hidden block"
             height={30}
           />
