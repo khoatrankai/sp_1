6a7353be0db8e877ae0d95d6affb6533c78bd625
VOY1511 <anhvi2898@gmail.com>
2025-10-26 16:18:22 +0700

    update

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -8,7 +8,6 @@ import { fetchTypeWork } from "@/redux/store/slices/activitySlices/type_work.sli
 import { fetchWorks } from "@/redux/store/slices/activitySlices/work.slide";
 import { fetchUserInfo } from "@/redux/store/slices/userSlices/get_users.slice";
 import { AppDispatch } from "@/redux/store/store";
-import useCheckRole from "@/utils/CheckRole";
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
